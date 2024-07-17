b18d32c9fdbacf09d96a8527f1eea346758f15a3
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-07-17 13:24:07 +0530

    mega blog

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -20,6 +20,13 @@ const authSlice = createSlice({
   },
 });
 
+/*STEPS to create post slice:
+update your store for post
+create slice for posts
+fetch all posts info in app.jsx and dispatch to store
+fetch post info from store and diplay in Home.jsx (use filter to filter out inactive posts)
+repeat 4 for all posts without filter*/
+
 export const { login, logout } = authSlice.actions;
 
 export default authSlice.reducer;
