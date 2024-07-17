642ad2d33d253f1173826b0758ee9a7a997b9629
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-07-17 13:05:36 +0530

    mega blog

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Select from "./Select";
+import Header from "./header/header";
+import Footer from "./footer/footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./Rte";
+import Signup from "./SignUp";
+import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./Card";
+import AuthLayout from "./AuthLayout";
+import Input from "./input";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  RTE,
+  Signup,
+  Login,
+  Button,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Input,
+  Select,
+};
